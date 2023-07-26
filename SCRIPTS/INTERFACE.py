dfae1630d782283903e9a3451ea3c9c48ff672a1
Kevyn <kevynsilveira0610@gmail.com>
2023-07-26 17:29:46 -0300

    Feito a implementação do campo de agente cobrador e finalizado o campo de selecionar a data de inicio e fim

--- SCRIPTS/INTERFACE.py
+++ SCRIPTS/INTERFACE.py
@@ -19,7 +19,7 @@ def create_main_frame(): # Cria a interface
 
     # Definindo parâmetros do frame main
     frame_main = ctk.CTk()
-    frame_main.geometry("500x300")
+    frame_main.geometry("310x270")
     frame_main.title("Correção FIDC")
     frame_main.resizable(False, False)
 
@@ -37,20 +37,20 @@ def create_main_frame(): # Cria a interface
             entry_start_date.insert(0, start_date)
             frame_select_date.destroy()
 
-        frame_select_date = ctk.CTkFrame(master=frame_main, width=480, height=280, corner_radius=8)
+        frame_select_date = ctk.CTkFrame(master=frame_main, width=290, height=255, corner_radius=8)
         frame_select_date.place(x=10, y=10)
 
         button_back = ctk.CTkButton(master=frame_select_date, width=100, height=30, corner_radius=8, text="Cancelar", command=back)
-        button_back.place(x=145, y=240)
+        button_back.place(x=45, y=220)
 
         button_confirm = ctk.CTkButton(master=frame_select_date, width=100, height=30, corner_radius=8, text="Confirmar", command=confirm)
-        button_confirm.place(x=255, y=240)
+        button_confirm.place(x=155, y=220)
 
-        label_warning = ctk.CTkLabel(master=frame_select_date, text="Selecione a data de inicio:")
-        label_warning.place(x=175, y=10)
+        label_start = ctk.CTkLabel(master=frame_select_date, text="Selecione a data de inicio:")
+        label_start.place(x=75, y=0)
 
         cal = Calendar(frame_select_date, selectmode="day", year=year, month=month)
-        cal.place(x=125, y=40)
+        cal.place(x=25, y=30)
 
     def select_final_date(): # Cria um frame para selecionar o mês e o dia
         nonlocal final_date
@@ -66,20 +66,20 @@ def create_main_frame(): # Cria a interface
             entry_final_date.insert(0, final_date)
             frame_select_date.destroy()
 
-        frame_select_date = ctk.CTkFrame(master=frame_main, width=480, height=280, corner_radius=8)
+        frame_select_date = ctk.CTkFrame(master=frame_main, width=290, height=255, corner_radius=8)
         frame_select_date.place(x=10, y=10)
 
         button_back = ctk.CTkButton(master=frame_select_date, width=100, height=30, corner_radius=8, text="Cancelar", command=back)
-        button_back.place(x=145, y=240)
+        button_back.place(x=45, y=220)
 
         button_confirm = ctk.CTkButton(master=frame_select_date, width=100, height=30, corner_radius=8, text="Confirmar", command=confirm)
-        button_confirm.place(x=255, y=240)
+        button_confirm.place(x=155, y=220)
 
-        label_warning = ctk.CTkLabel(master=frame_select_date, text="Selecione a data de fim:")
-        label_warning.place(x=175, y=10)
+        label_final = ctk.CTkLabel(master=frame_select_date, text="Selecione a data de fim:")
+        label_final.place(x=75, y=0)
 
         cal = Calendar(frame_select_date, selectmode="day", year=year, month=month)
-        cal.place(x=125, y=40)
+        cal.place(x=25, y=30)
 
     def create_confirmation_frame():
         nonlocal valor
@@ -88,21 +88,26 @@ def create_main_frame(): # Cria a interface
         def confirm():
             print("comando executado")
 
-        frame_confirmation = ctk.CTkFrame(master=frame_main, width=480, height=280, corner_radius=8)
+        frame_confirmation = ctk.CTkFrame(master=frame_main, width=290, height=255, corner_radius=8)
         frame_confirmation.place(x=10, y=10)
 
         label_warning = ctk.CTkLabel(master=frame_confirmation, text=valor)
         label_warning.place(x=50, y=50)
 
-        button_back = ctk.CTkButton(master=frame_confirmation, width=100, height=30, corner_radius=8, text= "Cancelar", command=back)
-        button_back.place(x=145, y=240)
+        button_back = ctk.CTkButton(master=frame_confirmation, width=100, height=30, corner_radius=8, text="Cancelar", command=back)
+        button_back.place(x=45, y=220)
 
-        button_confirm = ctk.CTkButton(master=frame_confirmation, width=100, height=30, corner_radius=8, text= "Confirmar", command=confirm)
-        button_confirm.place(x=255, y=240)
+        button_confirm = ctk.CTkButton(master=frame_confirmation, width=100, height=30, corner_radius=8, text="Confirmar", command=confirm)
+        button_confirm.place(x=155, y=220)
 
     def validate_input(char):
         # Verifica se o caractere é um dígito numérico
-        return char.isdigit()
+        if char.isdigit():
+            # Verifica o comprimento atual do texto no Entry
+            entry_text = entry_collection_agent.get()
+            if len(entry_text) < 4:
+                return True
+        return False
 
 
     button_start_date = ctk.CTkButton(master=frame_main, width=170, height=30, corner_radius=8, text="Selecione a data de inicio", command=select_start_date)
@@ -129,10 +134,17 @@ def create_main_frame(): # Cria a interface
 
     vcmd = frame_main.register(validate_input)
     entry_collection_agent.configure(validate="key", validatecommand=(vcmd, "%S"))
+    entry_collection_agent.configure(justify="center") # Centraliza o texto no entry
+
+    option_establishment = ["SC", "RS"]  # Opções de estabelecimento
+    combobox_establishment = ctk.CTkComboBox(master=frame_main, values=option_establishment, width=290, height=30, state="readonly")
+    combobox_establishment.set("---Selecione uma linguagem---") # Indica que precisa escolher um estabelecimento
+    combobox_establishment.configure(justify="center") # Centraliza o texto na combobox
+    combobox_establishment.place(x=10, y=140)
 
 
     button_select = ctk.CTkButton(master=frame_main, width=100, height=30, corner_radius=8, command=create_confirmation_frame, text= "Selecionar")
-    button_select.place(x=200, y=260)
+    button_select.place(x=105, y=235)
 
 
     frame_main.mainloop()
